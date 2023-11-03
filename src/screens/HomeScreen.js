fff5dc85fe77f67ace815899a4551bde068cd53b
soyukb <soyukoba@gmail.com>
2023-11-04 00:03:37 +0900

    コメントを入れる

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen!!!!!!</Text>;
+const HomeScreen = (props) => {
+  // console.log(props.navigation)
+  return <View>
+    <Text style={styles.text}>HomeScreen!!!!!!</Text>
+    <Button 
+    onPress = {()=>{props.navigation.navigate("Components")}}
+    title="Go to Compmemts Demo"/>
+    <Button 
+    onPress = {()=>{props.navigation.navigate("List")}}
+    title="Go to List Demo"/>
+    <TouchableOpacity onPress={()=>{props.navigation.navigate("List")}}>
+      <Text>Go tp List Demo</Text>
+    </TouchableOpacity>
+  </View>
 };
 
 const styles = StyleSheet.create({
